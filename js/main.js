ead73d4b46c920ac0dea89c878131420259bd3c9
fraguival <fran@fraguival.com>
2023-05-29 10:27:29 -0700

    add

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
-const iconMenu = document.querySelector('#icon-menu')
-const menu = document.querySelector('#menu')
+const ham = document.querySelector('.ham')
+const menu = document.querySelector('.menu')
 
-iconMenu.addEventListener('click', () => {
-    'use strict'
-    menu.classList.toggle('show')
+ham.addEventListener('click', () => {
+
+    ham.classList.toggle('gate')
+    menu.classList.toggle('look')
 })
 
-const app = document.getElementById('typewriter')
-
-    const typewriter = new Typewriter(app, {
-        loop: true,
-        delay: 75
-    })
-    typewriter
-        .typeString('La ciudad del tomate')
-        .pauseFor(200)
-        .start()
